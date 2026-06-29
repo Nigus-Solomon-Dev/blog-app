851a3cb19d7d7868070aaedafbc0ce0f83ca2516
NS <Nigus.Solomon.Dev@outlook.com>
2026-06-29 22:44:37 +0300

    add express server setup

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
+const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017")
+    .then(() => app.listen(8000, () => console.log('Server running on port 8000')))
+    .catch((err) => console.log(err));
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+});
+
+app.use('/blogs', blogRoutes);
+
+app.use((req, res) => {
+    res.status(404).render('404', { title: 'Page Not Found' });
+});
