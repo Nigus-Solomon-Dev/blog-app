f66637eddb23f9f95ad82e269652bd60a168441a
NS <Nigus.Solomon.Dev@outlook.com>
2026-07-09 17:39:32 +0300

    Update blog app: add favicon, MongoDB Atlas, and improve header

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const blogRoutes = require('./routes/blogRoutes');
 
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017")
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => app.listen(8000, () => console.log('Server running on port 8000')))
     .catch((err) => console.log(err));
 
